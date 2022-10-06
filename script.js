05abb0e9cb0427ff3dfffac76f65225198faf3e8
oksana <opashkovska85@gmail.com>
2022-10-06 22:57:06 +0200

    add cart sum calculations

--- script.js
+++ script.js
@@ -1,36 +1,28 @@
-/* === Fetch the Products from json file === */
-/* asynchronous function: the program will wait till the data is loaded and then proceed further  */
+// Fetch the Products from json file
+// asynchronous function: the program will wait till the data is loaded and then proceed further
 const getProducts = async () => {
-  /* try and catch block to handle errors */
+  // try and catch block to handle errors
   try {
     const results = await fetch("products.json");
     const data = await results.json();
     const products = data.products;
     return products;
   } catch (err) {
-    /* in case of failure to load the products data the error will be cought and displayed on console */
+    // in case of failure to load the products data the error will be cought and displayed on console
     console.log(err);
   }
 };
-/* getting access to DOM element with id ""products""*/
+// getting access to DOM element with id "products"
 const productsWrapper = document.getElementById("products");
-/* getting access to button "More" */
-// const loadButton = document.querySelector(".load-btn");
-
-let cart = [];
-let cartTotal = 0;
-function addToCart(id) {
-  const productDom = document.querySelector(`[data-id=item-${id}]`);
-  console.log("product Dom", productDom);
-  const productInCart = {
-    name: productDom.querySelector(".item-name").innerText,
-    price: parseInt(productDom.querySelector(".price").innerText.substring(1)),
-    quantity: parseInt(productDom.querySelector(".quantity").value),
-  };
-
-  let shippingMethod =
-    document.querySelector("[name=method]:checked").value || "";
-  console.log(shippingMethod);
+// getting access to button "Estimate Total"
+const button = document.getElementById("btn-estimate");
+// === Calculation of the order total sum ===
+/* in case of the button click, the program will calculate the total order sum as well as shipping costs, 
+total item in cart and total sum of all items */
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  // calculation of shipping costs based on the selected shipping method
+  let shippingMethod = document.querySelector("[name=method]:checked").value;
   let shippingCost;
   switch (shippingMethod) {
     case "hermes":
@@ -43,35 +35,45 @@ function addToCart(id) {
       shippingCost = 0;
       break;
   }
-  console.log("Ship costs", shippingCost);
-  let totalItemPrice = productInCart.price * productInCart.quantity;
-  console.log("totalItemPrice", totalItemPrice);
-  estimate = "$" + (totalItemPrice + shippingCost).toFixed(2);
-  productInCart.totalItemPrice = totalItemPrice;
-  cart.push(productInCart);
-  console.log("Cart", cart);
+
   const itemsInCart = cart.reduce((sum, obj) => {
     return sum + obj.quantity;
   }, 0);
-  console.log("Items in cart", itemsInCart);
-  const totalPrice = cart.reduce((sum, obj) => {
+  const totalItemsPrice = cart.reduce((sum, obj) => {
     return sum + obj.totalItemPrice;
   }, 0);
+  const totalSum = shippingCost + totalItemsPrice;
   document.getElementById("items-qnt").value = itemsInCart;
   document.getElementById("shipping-sum").value = "$" + shippingCost;
-  document.getElementById("total-sum").value = "$" + totalPrice;
+  document.getElementById("total-sum").value = "$" + totalSum;
+});
+
+let cart = [];
+
+function addToCart(id) {
+  const productDom = document.querySelector(`[data-id=item-${id}]`);
+  const productInCart = {
+    name: productDom.querySelector(".item-name").innerText,
+    price: parseInt(productDom.querySelector(".price").innerText.substring(1)),
+    quantity: parseInt(productDom.querySelector(".quantity").value),
+  };
+
+  let totalItemPrice = productInCart.price * productInCart.quantity;
+  productInCart.totalItemPrice = totalItemPrice;
+  cart.push(productInCart);
+  console.log("Cart", cart);
 }
 
-/* asynchronous function: the program will wait till DOM content has been loaded and then proceed further with displaying products  */
+// Display Products
+// asynchronous function: the program will wait till DOM content has been loaded and then proceed further with displaying products
 window.addEventListener("DOMContentLoaded", async function () {
   let products = await getProducts();
   products = products.filter((product) => product.category === "Motherboard");
   displayProductItems(products);
-  // loadData();
 });
 
-/* === Display Products === */
-/* arror function that received the array of items passed as a parameter, creates the new array with the nested div element containing the item data for each item.  */
+/* arror function that received the array of items passed as a parameter, 
+ creates the new array with the nested div element containing the item data for each item. */
 
 const displayProductItems = (items) => {
   let displayProduct = items.map(
@@ -99,12 +101,8 @@ const displayProductItems = (items) => {
   displayProduct = displayProduct.join("");
   productsWrapper.innerHTML = displayProduct;
 };
-function displayCart() {
-  cartDom.style.display = "block";
-  cartDom.innerHTML = "<div>Text</div>";
-  productsWrapper.style.display = "none";
-}
-/* ==== Filter Products ==== */
+
+//Filter Products
 let filters = document.querySelectorAll(".filters div");
 filters = [...filters];
 filters.forEach((filter) => {
